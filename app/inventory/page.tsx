1f206f89a5023bfe550fbc5f8b24df9a9873785d
Dustin Alandzes <dustin.alandzes@hp.com>
2025-06-12 11:50:44 -0500

    get drag and drop working

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,17 +1,35 @@
 'use client'
 
-import {DndContext} from '@dnd-kit/core';
+import {DndContext, DragEndEvent} from '@dnd-kit/core';
 import Inventory from "@/components/Inventory";
+import {useAppDispatch} from "@/components/AppContext";
 
 export default function Page() {
+    const dispatch = useAppDispatch()
+
+    function handleDragEnd(event: DragEndEvent): void {
+        const {active, over} = event;
+
+        if (over && over.data.current?.accepts.includes(active.data.current?.type)) {
+            dispatch({type: 'MOVE_ITEM', fromIndex: active.data.current?.inventoryIndex, toIndex: over.data.current?.inventoryIndex});
+        }
+    }
+
     return <div style={{
         display: "flex",
         flexDirection: "column",
     }}>
         <div style={{
             alignSelf: "center",
-        }}>{"Inventory"}</div>
-        <DndContext>
+        }}>
+            {"Inventory"}
+        </div>
+
+        {/* Adding id to DndContext prevents a hydration error */}
+        <DndContext
+            id="draggable-inventory"
+            onDragEnd={handleDragEnd}
+        >
             <Inventory/>
         </DndContext>
     </div>
